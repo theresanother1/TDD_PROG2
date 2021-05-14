6b5df55e56fcd197b86ecc70d8bf696d5e31b24a
theresanother1 <71547484+theresanother1@users.noreply.github.com>
2021-05-14 22:22:02 +0200

    minlength Test

--- src/test/java/at/ac/mypackage/ValidatePasswordTest.java
+++ src/test/java/at/ac/mypackage/ValidatePasswordTest.java
@@ -1,7 +1,9 @@
 package at.ac.mypackage;
 
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidatePasswordTest {
       private ValidatePassword testPassword;
@@ -12,6 +14,14 @@ public class ValidatePasswordTest {
     void setup(){
         testPassword = new ValidatePassword();
         testPassword2 = new ValidatePassword("xyz");
+    }
+
+    @Test
+    public void testPasswordLengthMinimum(){
+        testPassword.setMyPassword("WeAreTogether@inThis90");
+        testPassword2.setMyPassword("short");
+        int actual = testPassword2.checkPassword(testPassword2);
+        int expected = testPassword.checkPassword(testPassword);
     }
 
 
